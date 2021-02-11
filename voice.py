68049fcad559a3333d06082239a5bb477eec0681
Nathan <nathansquan@gmail.com>
2021-02-11 01:45:27 -0500

    for loop in voice.py to iterrate over list of phone numbers

--- voice.py
+++ voice.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
 from twilio.rest import Client
 from config import account_sid, auth_token, twi_num, voice_num
+from time import sleep
 
 client = Client(account_sid, auth_token)
 
-call = client.calls.create(
-        from_ = twi_num,
-        twiml = '<Response><Say>Turtle tank leak detected! Turtle tank leak detected!Say></Response>',
-        to = voice_num
-        )
-
-print(call.sid)
+# iterate through voice_num list of recipient phone numbers
+for i in voice_num:
+    call = client.calls.create(
+            from_ = twi_num,
+            twiml = '<Response><Say>Turtle tank leak detected! Please check for water. Turtle tank leak detected! Please check for water.</Say></Response>',
+            to = i
+            )
+    print(call.sid)
+    sleep(10)
